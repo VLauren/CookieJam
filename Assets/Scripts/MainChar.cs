4b4df494b4e268052bdfaa17241301963f10a94e
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2022-07-29 21:41:06 +0200

    rotacion de modelo

--- Assets/Scripts/MainChar.cs
+++ Assets/Scripts/MainChar.cs
@@ -5,19 +5,35 @@ using UnityEngine.InputSystem;
 
 public class MainChar : MonoBehaviour
 {
+    [SerializeField] float MovementSpeed = 4;
+    [SerializeField] float Gravity = 20;
+    [SerializeField] float ModelRotationSpeed = 360;
+
     Vector3 MoveInput;
     Vector3 ControlMovement;
     float VerticalVelocity;
 
+    Transform Model;
+
     void Start()
     {
-        
+        Model = transform.Find("Model");
     }
 
     void Update()
     {
-        ControlMovement = Vector3.right * MoveInput.x * Time.deltaTime * 3;
+        ControlMovement = Vector3.right * MoveInput.x * Time.deltaTime * MovementSpeed;
         GetComponent<CharacterController>().Move(ControlMovement + new Vector3(0, VerticalVelocity, 0));
+
+        if (GetComponent<CharacterController>().isGrounded)
+            VerticalVelocity = -1;
+        else
+            VerticalVelocity -= Time.deltaTime * Gravity;
+
+        if (MoveInput.x == 1)
+            Model.rotation = Quaternion.RotateTowards(Model.rotation, Quaternion.Euler(90, 0, -95), Time.deltaTime * ModelRotationSpeed);
+        if (MoveInput.x == -1)
+            Model.rotation = Quaternion.RotateTowards(Model.rotation, Quaternion.Euler(90, 0, 95), Time.deltaTime * ModelRotationSpeed);
     }
 
     void OnMove(InputValue value)
