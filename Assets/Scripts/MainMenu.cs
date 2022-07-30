d49ea0adc14d048112e60aaabd5196d44a17bcb5
ByteHunter <info@bytehunter.net>
2022-07-30 18:05:58 +0200

    Menú principal, historia y controles con placeholders

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,8 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Panel Groups")]
+    [SerializeField] GameObject mainMenuGroup;
+    [SerializeField] GameObject storyGroup;
+    [SerializeField] GameObject controlsInfoGroup;
+
+    bool storyShown;
+    bool controlsShown;
+
+    private void Start()
+    {
+        storyShown = false;
+        controlsShown = false;
+    }
+
     void OnAnyKey(InputValue value)
     {
+        if (!storyShown)
+        {
+            Debug.Log("Showing the story");
+            ShowPanel(storyGroup);
+            storyShown = true;
+            return;
+        }
+        if (!controlsShown)
+        {
+            Debug.Log("Showing the controls");
+            ShowPanel(controlsInfoGroup);
+            controlsShown = true;
+            return;
+        }
+        Debug.Log("Loading first level");
         CJGame.LoadFirstLevel();
+    }
+
+    private void HideAllPanels()
+    {
+        mainMenuGroup.SetActive(false);
+        storyGroup.SetActive(false);
+        controlsInfoGroup.SetActive(false);
+    }
+
+    private void ShowPanel(GameObject panel)
+    {
+        HideAllPanels();
+        panel.SetActive(true);
     }
 }
