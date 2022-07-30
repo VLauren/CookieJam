843473aa437b7ee5cde06e6a0a81d5fa595e2c13
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2022-07-30 19:48:24 +0200

    nivel 0 de prueba grafica, ajustes

--- Assets/Scripts/DamageTrigger.cs
+++ Assets/Scripts/DamageTrigger.cs
@@ -15,4 +15,14 @@ public class DamageTrigger : MonoBehaviour
             other.GetComponent<MainChar>().ApplyDamage(ReturnToLastSafePos, IgnoreInvul, PlaySpikeSound);
         }
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+
+        Gizmos.color = new Color(1, 0.5f, 0.5f, 0.5f);
+        Gizmos.DrawCube(Vector3.zero, Vector3.one);
+        Gizmos.color = new Color(1, 0, 0, 0.7f);
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
 }
