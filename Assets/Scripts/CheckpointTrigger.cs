52884254349df326461e6532d11dcfb02eaad51f
Laurentino Vilchez <laurentino.vilchez@gmail.com>
2022-07-30 21:23:18 +0200

    checkpoint brilli brilli

--- Assets/Scripts/CheckpointTrigger.cs
+++ Assets/Scripts/CheckpointTrigger.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class CheckpointTrigger : MonoBehaviour
 {
+    GameObject Glow;
+
+    private void Start()
+    {
+        Glow = transform.Find("Glow").gameObject;
+        Glow.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.GetComponent<MainChar>() != null)
         {
-            other.GetComponent<MainChar>().SetNewCheckpoint(transform);
+            if (Glow.activeSelf == false)
+            {
+                other.GetComponent<MainChar>().SetNewCheckpoint(transform);
+
+                // Apago todos
+                foreach (var cp in FindObjectsOfType<CheckpointTrigger>())
+                    cp.ShowGlow(false);
+
+                // Brillo yo
+                ShowGlow(true);
+            }
         }
+    }
+
+    public void ShowGlow(bool _show)
+    {
+        Glow.SetActive(_show);
     }
 }
